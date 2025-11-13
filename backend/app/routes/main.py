ff30ec0368ccaef7a149b1c39aad495eacd74d06
Ian Santos <issmovel@gmail.com>
2025-11-13 10:51:04 -0300

    update

--- backend/app/routes/main.py
+++ backend/app/routes/main.py
@@ -1,15 +1,14 @@
 from flask import Blueprint, jsonify
 from datetime import datetime
 
-# Cria um "Módulo" (Blueprint) chamado 'main'
 bp = Blueprint('main', __name__)
 
-@bp.route('/')
+@bp.route('/') # Esta rota agora será /api/
 def index():
     """ Rota principal apenas para teste """
     return jsonify({"message": "API Gestão Transportes no ar! (Estrutura de Módulos)"})
 
-@bp.route('/api/status')
+@bp.route('/status') # ROTA ALTERADA: de '/api/status' para '/status'
 def status():
     """ Rota de status da API """
     return jsonify({"status": "OK", "timestamp": datetime.utcnow()})
